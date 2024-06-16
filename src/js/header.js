e28bc2b5b93c5c6831b9e6c0280b1f33cc4842f1
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-16 12:05:36 +0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,7 @@ a.forEach(el => {
   const hreflink = '/' + el.firstElementChild.getAttribute('href');
   console.log(hreflink);
   console.log(window.location.pathname);
+  console.log(el.firstElementChild.getAttribute('href'));
   if (hreflink === window.location.pathname) {
     el.classList.add('nav-item-active');
   }
