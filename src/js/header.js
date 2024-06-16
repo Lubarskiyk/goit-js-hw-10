a4540ccd8b40b386a27de1431852ca962f90696f
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-16 16:29:04 +0300

    Update header.js

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,11 @@
 let a = document.querySelectorAll('.nav-item');
 a.forEach(el => {
   const hreflink = el.firstElementChild.getAttribute('href');
-  const pathName = window.location.pathname.split('/').pop();
+  console.log(window.location.pathname.split('/').pop().length);
+  const pathName =
+    window.location.pathname.split('/').pop().length === 0
+      ? 'index.html'
+      : window.location.pathname.split('/').pop();
 
   console.log(pathName);
   console.log(el.firstElementChild.getAttribute('href'));
