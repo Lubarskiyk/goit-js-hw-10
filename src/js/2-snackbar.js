5f20427c93708af209d48eb4356668b175d51a5f
Lubarskiy Konstantin <lubarskiy.k@gmail.com>
2024-06-17 14:18:32 +0300

    45

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -11,3 +11,19 @@ iziToast.settings({
   messageSize: 25,
   backgroundColor: 'rgba(26, 255, 128, 0.8)',
 });
+
+const counterBottonEl = document.querySelector('.counter');
+const inputDelayEl = document.querySelector('.timer-input');
+
+counterBottonEl.addEventListener('click', event => {
+  let currentDelay =
+    inputDelayEl.value === '' ? 0 : parseInt(inputDelayEl.value);
+  if (event.target.textContent === '+') {
+    console.log('+');
+    currentDelay += 1;
+    inputDelayEl.value = currentDelay;
+  } else if (event.target.textContent === '-') {
+    currentDelay = currentDelay === 0 ? 0 : (currentDelay -= 1);
+    inputDelayEl.value = currentDelay;
+  }
+});
--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,6 @@
 const headerNavigation = document.querySelectorAll('.nav-item');
 headerNavigation.forEach(el => {
   const hreflink = el.firstElementChild.getAttribute('href');
-  console.log(window.location.pathname.split('/').pop().length);
   const pathName =
     window.location.pathname.split('/').pop().length === 0
       ? 'index.html'
